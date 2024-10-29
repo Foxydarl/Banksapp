dd39ebc48689a7431fc7cf6fd2809993f7e711d3
Foxy_dark <boris.panna@mail.ru>
2024-10-29 21:37:49 +0600

    Завершил интро с кодом в MainActivity

--- app/src/main/java/com/example/banksapp/MainActivity.java
+++ app/src/main/java/com/example/banksapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.banksapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,25 +26,21 @@ public class MainActivity extends AppCompatActivity {
     View view1 = findViewById(R.id.indicator1);
     View view2 = findViewById(R.id.indicator2);
     View view3 = findViewById(R.id.indicator3);
+    ImageView imageView= findViewById(R.id.introView);
+    TextView maintext = findViewById(R.id.mainText);
+    TextView subtext = findViewById(R.id.subText);
     float density = getResources().getDisplayMetrics().density;
     int dpValue = (int) (view1.getWidth() / density + 0.5f);
     int view3Value = (int) (view3.getWidth() / density + 0.5f);
     if (view3Value == 16){
-        ViewGroup.LayoutParams view1params= view1.getLayoutParams();
-        view1params.width = (int) (8 * density + 0.5f);
-        view1.setLayoutParams(view1params);;
-        view1.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_unselected, null));
-        ViewGroup.LayoutParams view2params= view2.getLayoutParams();
-        view2params.width = (int) (16 * density + 0.5f);
-        view2.setLayoutParams(view2params);
-        view2.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_selected, null));
-        ViewGroup.LayoutParams view3params= view3.getLayoutParams();
-        view3params.width = (int) (8 * density + 0.5f);
-        view3.setLayoutParams(view3params);
-        view3.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_unselected, null));
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
     }
 
     else if (dpValue == 16){
+        maintext.setText("The most Secoure Platfrom for Customer");
+        subtext.setText("Built-in Fingerprint, face recognition and more, keeping you completely safe");
+        imageView.setImageResource(R.drawable.introsecond);
         ViewGroup.LayoutParams view1params= view1.getLayoutParams();
         view1params.width = (int) (8 * density + 0.5f);
         view1.setLayoutParams(view1params);;
@@ -56,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
         view3.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_unselected, null));
         }
     else if (dpValue == 8){
+        maintext.setText("Paying for Everything is Easy and Convenient");
+        subtext.setText("Built-in Fingerprint, face recognition and more, keeping you completely safe");
+        imageView.setImageResource(R.drawable.introthird);
         ViewGroup.LayoutParams view1params= view1.getLayoutParams();
         view1params.width = (int) (8 * density + 0.5f);
         view1.setLayoutParams(view1params);;
@@ -63,11 +65,11 @@ public class MainActivity extends AppCompatActivity {
         ViewGroup.LayoutParams view2params= view2.getLayoutParams();
         view2params.width = (int) (8 * density + 0.5f);
         view2.setLayoutParams(view2params);
-        view2.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_selected, null));
+        view2.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_unselected, null));
         ViewGroup.LayoutParams view3params= view3.getLayoutParams();
         view3params.width = (int) (16 * density + 0.5f);
         view3.setLayoutParams(view3params);
-        view3.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_unselected, null));
+        view3.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.indicator_selected, null));
     }
 
 
