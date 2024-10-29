fee0d6878562f779becbc68a652a09c7848c32c0
Foxy_dark <boris.panna@mail.ru>
2024-10-29 15:58:12 +0600

    Сделанная первая страница интро

--- app/src/main/java/com/example/banksapp/MainActivity.java
+++ app/src/main/java/com/example/banksapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.banksapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,4 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         };
+
+    public void onNextClick(View view) {
+    View view1 = findViewById(R.id.indicator1);
+    View view2 = findViewById(R.id.indicator2);
+    View view3 = findViewById(R.id.indicator3);
+    if (view1.getWidth() == 16){
+        ViewGroup.LayoutParams view1params= view1.getLayoutParams();
+        view1params.width = 8;
+
+        }
+
+
+    }
     }
